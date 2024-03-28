840b3e6ad2c48692844cda2863c22e13bdea7da3
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-03-28 15:31:11 -0700

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that takes in a URL, sends a request to the URL
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    rqst = urllib.request.Request(url)
+    with urllib.request.urlopen(rqst) as response:
+        print(dict(response.headers).get("X-Request-Id"))
